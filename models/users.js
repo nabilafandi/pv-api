682bc263300f506bfc00298991a3113949d104a8
nabilafandi <nabilafandih@gmail.com>
2023-02-24 13:34:35 +0700

    singkat nama data dan menghapus data yang generate

--- models/users.js
+++ models/users.js
@@ -8,41 +8,41 @@ const Users = mongoose.Schema({
         type: String,
         require: true
     },
-    alamat_user: {
+    alamat: {
         type: String,
     },
-    no_hp_user: {
+    no_hp: {
         type: String,
     },
-    saldo_user: {
+    saldo: {
         type: Number,
         require: true,
         default: 0
     },
-    pengeluaran_user: {
-        type: Number,
-    },
-    total_pengeluaran_user: {
-        type: Number,
-    },
-    estimasi_pengeluaran_harian: {
-        type: Number,
-    },
-    estimasi_pengeluaran_bulanan: {
-        type: Number,
-    },
-    estimasi_pengeluaran_tahunan: {
-        type: Number,
-    },
-    total_penghematan_harian: {
-        type: Number,
-    },
-    total_penghematan_bulanan: {
-        type: Number,
-    },
-    total_penghematan_tahunan: {
-        type: Number,
-    }
+    // pengeluaran: {
+    //     type: Number,
+    // },
+    // total_pengeluaran_user: {
+    //     type: Number,
+    // },
+    // estimasi_pengeluaran_harian: {
+    //     type: Number,
+    // },
+    // estimasi_pengeluaran_bulanan: {
+    //     type: Number,
+    // },
+    // estimasi_pengeluaran_tahunan: {
+    //     type: Number,
+    // },
+    // total_penghematan_harian: {
+    //     type: Number,
+    // },
+    // total_penghematan_bulanan: {
+    //     type: Number,
+    // },
+    // total_penghematan_tahunan: {
+    //     type: Number,
+    // }
 })
 const User = mongoose.model('user', Users)
 
