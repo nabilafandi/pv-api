c7c0996a2eef022f0b4b6779ea0db4795273adc4
nabilafandi <nabilafandih@gmail.com>
2023-03-02 13:52:17 +0700

    validasi plant

--- controllers/sensor/aggregateSensor.js
+++ controllers/sensor/aggregateSensor.js
@@ -1,16 +1,19 @@
+const { findByPlantId } = require('../../services/plant.service')
 const services = require('../../services/services')
 
 const aggregate = async (req, res) => {
-    const days = req.body.days
+    const days = req.body.time
     const userId = req.body.userId
-    if (userId) return 
 
     try {
-
-        const aggregated = await services.sensor.aggregateSensor(days,userId)
+        //cekuserid kosong
+        if (!userId) return res.status(400).json(error)
+        const cekplant_id = await findByPlantId(userId)
+        console.log(cekplant_id)
+        const aggregated = await services.sensor.aggregateSensor(days, userId)
         res.status(200).json({ aggregated })
     } catch (error) {
-        res.status(400).json({ error})
+        res.status(400).json({ error })
     }
 }
 
