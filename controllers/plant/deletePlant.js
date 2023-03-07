5cfea4b3951501527d4245ebf057942f6bd2e5e6
nabilafandi <nabilafandih@gmail.com>
2023-03-07 12:46:47 +0700

    objectid validation

--- controllers/plant/deletePlant.js
+++ controllers/plant/deletePlant.js
@@ -1,15 +1,26 @@
 const services = require('../../services/services')
+const mongoose = require('mongoose');
+const validation = (id) => {
+  const idObject = mongoose.Types.ObjectId;
+  if (idObject.isValid(id)) {
+    return (String)(new Object(id)) === id;
+  }
+  return false;
+
+}
+
 
 const deletePlant = async (req, res) => {
-    try {
-      const deletedPlant = await services.plant.deletebyId(req.params.id)
-      console.log(deletedPlant)
-      if (!deletedPlant) { res.status(404).json({ error: "Plant not Found" }) }
-      else {
-        res.json({ message: "Plant deleted succesfully.", deletedPlant})
-      }
-    } catch (error) {
-      res.status(500).json({ error: "Internal error."})
+  try {
+    //validasi id
+    if (!validation(req.params.id)) return res.status(400).json({ error: "plant object id is not valid" })
+    const deletedPlant = await services.plant.deletebyId(req.params.id)
+    if (!deletedPlant) { res.status(404).json({ error: "Plant not Found" }) }
+    else {
+      res.json({ message: "Plant deleted succesfully." })
     }
+  } catch (error) {
+    res.status(500).json({ error: "Internal error." })
   }
+}
 module.exports = deletePlant
--- controllers/plant/updatePlant.js
+++ controllers/plant/updatePlant.js
@@ -1,17 +1,27 @@
 const services = require('../../services/services')
 
-const updatePlant =  async (req, res) => {
+const mongoose = require('mongoose');
+const validation = (id) => {
+  const idObject = mongoose.Types.ObjectId;
+  if (idObject.isValid(id)) {
+    return (String)(new Object(id)) === id;
+  }
+  return false;
+
+}
+
+const updatePlant = async (req, res) => {
   data = req.body
   try {
     //validasi
     const { error } = services.validation.updatePlant.validate(data)
     if (error) return res.status(400).json({ error: error.details[0].message })
-
-
+    //validasi id
+    if (!validation(req.params.id)) return res.status(400).json({ error: "plant object id is not valid" })
     const updatedPlant = await services.plant.updatePlant(req.params.id, data)
     if (!updatedPlant) { res.status(404).json({ error: "Plant not Found" }) }
     else {
-      res.json({ message: "Plant updated succesfully.", updatedPlant })
+      res.json({ message: "Plant updated successfully." })
     }
   } catch (error) {
     res.status(500).json({ error: "Internal error." })
