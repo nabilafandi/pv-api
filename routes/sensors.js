5db551d0fe09171bfdedc7f36f44f1cd61093478
nabilafandi <nabilafandih@gmail.com>
2023-03-10 15:22:56 +0700

    added accesstoken requirement to all sensor endpoint

--- routes/sensors.js
+++ routes/sensors.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const Sensor = require('../controllers/sensor.controller');
+const cekToken = require('../middleware/verifyJWT')
 
 const Sensors = express.Router()
 
 //Sensor
 Sensors.route('/')
-    .post(Sensor.addSensor)
-    .get(Sensor.aggregate)
+    .post(cekToken.verifyJWT,Sensor.addSensor)
+    .get(cekToken.verifyJWT,Sensor.aggregate)
 Sensors.route('/latest')
-    .get(Sensor.latestData)
+    .get(cekToken.verifyJWT,Sensor.latestData)
 
 module.exports = Sensors
