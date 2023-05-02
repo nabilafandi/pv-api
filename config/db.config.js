434e7c5d68a6e46ddad2c01473c42128398ecdf2
nabilafandi <nabilafandih@gmail.com>
2023-05-02 10:53:42 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: "202.148.1.57",
+    HOST: "127.0.0.1",
     PORT: 27017,
     DB: "new_pv",
-    user: "pvXy123",
-    password: "HBi8agY1XFO25aaWs0LpWS",
+    // user: "nabil",
+    // password: "123456",
   };
