023b9871811af7a9b3847065aab21110dbbe9e1b
nabilafandi <nabilafandih@gmail.com>
2023-05-02 16:19:35 +0700

    added option to work with local mongodb instance

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,13 @@
+// module.exports = {
+//   HOST: "202.148.1.57",
+//   PORT: 27017,
+//   DB: "new_pv",
+//   user: "pvXy123",
+//   password: "HBi8agY1XFO25aaWs0LpWS",
+// };
 module.exports = {
-    HOST: "127.0.0.1",
-    PORT: 27017,
-    DB: "new_pv",
-    // user: "nabil",
-    // password: "123456",
-  };
+HOST: "127.0.0.1",
+PORT: 27017,
+DB: "new_pv",
+};
+
